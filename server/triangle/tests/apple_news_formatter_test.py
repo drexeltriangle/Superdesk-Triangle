33e3e04144d3ce01c3760370321be1c142820176
Brandon <brandon.kolle@thetriangle.org>
2019-12-04 00:50:07 +0000

    Added first tests for apple_news_formatter and started .gitignore

--- server/triangle/tests/apple_news_formatter_test.py
+++ server/triangle/tests/apple_news_formatter_test.py
@@ -3,9 +3,10 @@
 from superdesk.tests import TestCase
 from triangle.publish.formatters.triangle_apple_news_formatter import TriangleAppleNewsFormatter
 
-class TriangleAppleNewsFormatterTest(TestCase):
+class TriangleAppleNewsFormatterTests(TestCase):
     def setUp(self):
         self.formatter = TriangleAppleNewsFormatter()
         self.maxDiff = None
-
     
+    def test_type_is_apple_news(self):
+        self.assertEqual("Triangle Apple News", self.formatter.format_type)
